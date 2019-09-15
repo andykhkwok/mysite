6fbc548f210485b64ccc9a560f0c52a40108a474
Andy Kwok <andykhkwok@gmail.com>
2019-09-15 00:02:44 -0700

    fix mistake from lesson 6

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,9 +1,14 @@
 # blogging/urls.py
 
 from django.urls import path
-from blogging.views import list_view, detail_view
+from blogging.views import stub_view, list_view
 
 urlpatterns = [
-    path('', list_view, name="post_index"),
-    path('posts/<int:post_id>/', detail_view, name="post_detail"),
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
+
+# urlpatterns = [
+    # path('', stub_view, name="blog_index"),
+    # path('posts/<int:post_id>/', stub_view, name="blog_detail"),
+# ]
--- blogging/views.py
+++ blogging/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.template import loader
 from blogging.models import Post
 
 def list_view(request):
-    context = {'posts': Post.objects.all()}
-    return render(request, 'blogging/list.html', context)
+    # published = Post.objects.exclude(published_date__exact=None)
+    # posts = published.order_by('-published_date')
+    # context = {'posts': posts}
+    # return render(request, 'list.html', context)
+
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
+    
+    # context = {'posts': Post.objects.all()}
+    # return render(request, 'blogging/list.html', context)
 
 def detail_view(request, post_id):
     try:
@@ -24,3 +37,13 @@ def detail_view(request, post_id):
         
     context = {'post': post}
     return render(request, 'posting/detail.html', context)
+
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
