93da48235c1935cbae1ad8aa15f1913804d7867b
Andy Kwok <andykhkwok@gmail.com>
2019-09-15 20:05:46 -0700

    Activity 7

--- blogging/views.py
+++ blogging/views.py
@@ -7,36 +7,26 @@ from django.template import loader
 from blogging.models import Post
 
 def list_view(request):
-    # published = Post.objects.exclude(published_date__exact=None)
-    # posts = published.order_by('-published_date')
-    # context = {'posts': posts}
-    # return render(request, 'list.html', context)
-
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
-    
-    # context = {'posts': Post.objects.all()}
-    # return render(request, 'blogging/list.html', context)
+    return render(request, 'blogging/list.html', context)
 
+    # published = Post.objects.exclude(published_date__exact=None)
+    # posts = published.order_by('-published_date')
+    # template = loader.get_template('blogging/list.html')
+    # context = {'posts': posts}
+    # body = template.render(context)
+    # return HttpResponse(body, content_type="text/html")
+    
 def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
     try:
-        post = Post.objects.get(pk=post_id)
+        post = published.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404
-    
-    if request.method == "POST":
-        if request.POST.get("vote") == "Yes":
-            post.score += 1
-        else:
-            post.score -= 1
-        post.save()
-        
     context = {'post': post}
-    return render(request, 'posting/detail.html', context)
+    return render(request, 'blogging/detail.html', context)
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
